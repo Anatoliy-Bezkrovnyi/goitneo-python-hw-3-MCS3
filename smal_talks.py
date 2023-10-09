027b2d0282dfe846f84d26eea872841f8708d9d7
Anatoliy-Bezkrovnyi <bezkrovnyi1@gmail.com>
2023-10-09 13:44:20 +0300

    module05 is added

--- smal_talks.py
+++ smal_talks.py
@@ -1,31 +1,16 @@
-def format_ingredients(items):
-   formatted_string = ""
-   if len(items) == 0:
-       return formatted_string
-   elif len(items) == 1:
-       formatted_string = formatted_string + items[0]
-       return formatted_string
-   elif len(items) == 2:
-       items.insert(len(items) -1, " and ")
-       for item in items:
-           formatted_string = formatted_string + item
-       return formatted_string
-   else:
-       items.insert(len(items) -1, " and ")
-       counter = 0
-       while counter < len(items) - 2:
-           if counter % 2 == 1:
-               items.insert(counter, ", ")
-               counter = counter + 1
-           else:
-               counter = counter + 1   
-       
-       for item in items:
-           formatted_string = formatted_string + item
-       return formatted_string
-       
+def is_check_name(fullname, first_name):
+    
+    res = fullname.removeprefix(first_name)
+    print(res)
+    if res != fullname:
+        return True
+    else:
+        return False
+   
 
-list = ["sugar", "eggs", "tea", "vine", "crops", "garlic", "tomato", "pickles", "toasts", "chocolate"]
 
-print(format_ingredients(list))
-print(1%2)
+    
+
+
+#text = "Alex\nKdfe23\t\f\v.\r"
+print(is_check_name("Test Account", "Test"))
