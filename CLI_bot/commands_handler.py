4f9debf325133879e3e96dc16cd21563c7843bbb
Anatoliy-Bezkrovnyi <bezkrovnyi1@gmail.com>
2023-10-23 21:27:00 +0300

    Task 2 is added to home tasks

--- CLI_bot/commands_handler.py
+++ CLI_bot/commands_handler.py
@@ -5,18 +5,31 @@ def parse_input(user_input):
     return cmd, *args
 
 def add_contact(args, contacts):
-    name, phone = args
-    contacts[name] = phone
-    return "Contact added."
+    try:
+        name, phone = args
+        if name in contacts:
+            return "Name already exists, chose anther name or use 'change' command to update existing phone number"          
+        else:
+            contacts[name] = phone
+            return "Contact added."
+    except ValueError:
+        return "Contact name and phone are expected. Please try again"
 
 def change_contact(args, contacts):
-    name, phone = args
-    contacts[name] = phone
-    return "Contact updated."
+    try:
+        name, phone = args
+        contacts[name] = phone
+        return "Contact updated."
+    except ValueError:
+        return "Contact name and phone are expected. Please try again"
 
 def show_phone(args, contacts):
-    phone = contacts.get(args[0])
-    return phone
+    try:
+        name = args[0]
+        phone = contacts[name]
+        return phone
+    except ValueError:
+        return "Contact name is expected. Please try again"
 
 def show_all(contacts):
     for key, value in contacts.items():
