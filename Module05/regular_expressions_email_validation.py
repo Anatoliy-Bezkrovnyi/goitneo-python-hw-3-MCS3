b6c2877dc657f2e4364694ba6051c552b7837e21
Anatoliy-Bezkrovnyi <51107235+Anatoliy-Bezkrovnyi@users.noreply.github.com>
2023-10-29 19:41:27 +0200

    Delete Module05 directory
    Obsolete code

--- Module05/regular_expressions_email_validation.py
+++
@@ -1,9 +0,0 @@
-import re
-
-
-def find_all_emails(text):
-    result = re.findall(r"[a-zA-Z]{1}[a-zA-Z0-9_.]+[@][a-zA-z]+[.][a-zA-z]{2,}", text)
-    return result
-
-text = "Ima.Fool@iana.org Ima.Fool@iana.o 1Fool@iana.org first_last@iana.org first.middle.last@iana.or a@test.com abc111@test.com.net"
-print(find_all_emails(text))
